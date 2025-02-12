09647a731f60077bd8cf243d4fced4d9a464550c
TessWebDev <tdoyle@mcad.edu>
2025-02-12 00:14:17 -0600

    adding more shapes

--- week3/3_A/3_A.js
+++ week3/3_A/3_A.js
@@ -1,15 +1,25 @@
-let rotateBy = 5;
+let rotateBy = 8;
 function setup() {
     createCanvas(600, 600);
     background(0); // leaving the background here vs draw so it does not get wiped out when drawing is repeated.
     angleMode(DEGREES);
+    //text message
+    textFont('helvetica');
+    textSize(48);
+    fill("pink");
+    text('Click to Complete Drawing');
 }
-function makeArm(rotateBy) {
+function makeArm(rotateBy, stroke1, stroke2) {
     let alt = Math.round(rotateBy / 360);
+    console.log(alt);
+    //Large Bezier
     noFill();
     stroke(255);
     strokeWeight(1);
-    ellipse(150, 150 + alt, 150 - alt); // the coordinates are from the center --- Why does this have to be below the other items?
+    //ellipse(150, 150 - alt, 150 / alt); // the coordinates are from the center --- can play around with #'s to make shapes more interesting
+    bezier(alt - 10, alt * 10, alt + 400, alt - 10, alt * 100, 20, 40, 40)
+
+
 }
 
 function draw() {
@@ -17,5 +27,8 @@ function draw() {
     rotate(rotateBy); //come back to this .... This is rotating by degrees
     makeArm(rotateBy);
     rotateBy += 5;
+}
+function mousePressed() {
+    noLoop();
 }
 
