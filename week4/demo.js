573ef3388ba74c0e764c455982d2aff030aeceac
TessWebDev <tdoyle@mcad.edu>
2025-02-11 19:12:31 -0600

    What is DOM

---
+++ week4/demo.js
@@ -0,0 +1,4 @@
+//HTML -- Bones, CSS-- Skin, Java script -nervouse system
+//what is DOM
+console.log(window.document.body.children); //.children (an array) lets you see all of the items (attributes, etc.) in the body of your HTML
+//In DOM scripting you are using 
