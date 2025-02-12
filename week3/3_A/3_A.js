1b0fb66ff002fde908e0ff640024aea44dc6626a
TessWebDev <tdoyle@mcad.edu>
2025-02-11 23:42:02 -0600

    Expanding the circles of the spiro

--- week3/3_A/3_A.js
+++ week3/3_A/3_A.js
@@ -4,15 +4,18 @@ function setup() {
     background(0); // leaving the background here vs draw so it does not get wiped out when drawing is repeated.
     angleMode(DEGREES);
 }
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+    ellipse(150, 150 + alt, 150 - alt); // the coordinates are from the center --- Why does this have to be below the other items?
+}
 
 function draw() {
     translate(300, 300); //resets the center
     rotate(rotateBy); //come back to this .... This is rotating by degrees
-    
-    noFill();
-    stroke(255);
-    strokeWeight(1);
-    ellipse(150, 150, 150); // the coordinates are from the center --- Why does this have to be below the other items?
+    makeArm(rotateBy);
     rotateBy += 5;
 }
 
