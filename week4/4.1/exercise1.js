369acf3feb5730412a57696f5934ed46556a4cb7
TessWebDev <tdoyle@mcad.edu>
2025-02-16 16:57:14 -0600

    Complete exercise

--- week4/4.1/exercise1.js
+++ week4/4.1/exercise1.js
@@ -1,12 +1,15 @@
 //Select empty div and assign it to a variable.
-const myEmptyDiv = document.querySelector("myEmptyDiv");
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // 2 STEP PROCESS:
 // 1. Put an h1 inside of it 
  //create a new element
-const heading= document.createElement('h1');
-heading.innerHTML = 'Hello Houston! It is a hot and humid day!';
-
+const heading = document.createElement('h1');
+heading.innerHTML = 'Baby llamas: A baby llama is called a cria, which is Spanish for "child".';
+heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
-
+function handleHeadingClick() {
+    console.log('heading has been clicked');
+    document.body.style.backgroundColor = 'yellow';
+}
 
