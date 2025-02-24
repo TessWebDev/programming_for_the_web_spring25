57a384a9f73ee6ed6ab35876f385b7e6b8989ee5
TessWebDev <tdoyle@mcad.edu>
2025-02-24 00:32:01 -0600

    fixed flip

--- week5/memorygame.js
+++ week5/memorygame.js
@@ -1,20 +1,41 @@
-let myCard;
+
 const DOWN = 'down'; //const because it will not change
 const UP = 'up';
+let startingX = 100;
+let startingY = 100;
+let cards = []; //start an array to push all cards into
+const gameState = {
+
+};
 function setup() {
     createCanvas(800, 600);
     background(0);
-    myCard = new Card();
+    for (let j = 0; j < 2; j++) { //this is the loop to create a row.
+        for (let i = 0; i < 5; i++) {
+            cards.push(new Card(startingX, 
+            startingY)); // new Card is the new instance (or the cookie)
+            startingX += 120; // increment starting X 
+        }
+        //outter loop
+        startingY += 150; //the 120 is the space between the rows
+        startingX = 100;  //will get weird behavior unless you add this startingX back to what it was.
+
+    }
+
 }
 
 function mousePressed() {
-    console.log(myCard.didHit(mouseX, mouseY));
+    for (let k = 0; k < cards.length; k++) {
+        if(cards [k].didHit(mouseX, mouseY)) {
+            console.log('flipped');
+        }
+    }
 }
 
 class Card {
-    constructor () {
-        this.x = 100; //referenceing the x position, this is a property -- like variables but specific to your class
-        this.y = 100;
+    constructor (x, y) { //make parameters in ()
+        this.x = x; //referenceing the x position, this is a property -- like variables but specific to your class
+        this.y = y;
         this.width = 80;
         this.height = 100;
         this.face = DOWN; //the cards starting out as face down
@@ -26,7 +47,7 @@ class Card {
             fill('rgb(57.7%, 9.9%, 9.9%)');
             rect(this.x, this.y, this.width, this.height, 10);
         } else {
-            fill('#aaa');
+            fill('#aaa'); //the back of card
             rect(this.x, this.y, this.width, this.height, 10);
         }
         
