b4c136ad4469eae9c45add571a917171df890ce7
TessWebDev <tdoyle@mcad.edu>
2025-04-29 00:11:18 -0500

    finish ex2

--- week10/ex2/ex2/src/views/Song.jsx
+++ week10/ex2/ex2/src/views/Song.jsx
@@ -1,17 +1,26 @@
 import {Link, useParams} from "react-router-dom";
-import PropTypes from "prop-types";
-export function Song ({data}) {
+
+
+export function Song({data}) {
     const {slug} = useParams();
     console.log("slug", slug);
     const selectedSong = data.find((song) => song.slug === slug)
+    console.log(selectedSong)
 
     return (
         <>
         <nav>
             <Link to="/">Go Back Home</Link>
         </nav>
-        <h1>Song</h1>
+        <h1>{selectedSong.title}</h1>
+        <img src={selectedSong.cover} alt={selectedSong.title} />
+        <p>Artist: {selectedSong.artist}</p>
+        <p>Rank: {selectedSong.rank}</p>
         </>
        
     )
+}
+
+Song.proptypes = {
+    data: PropTypes.arrayOf(PropTypes.any)
 }
